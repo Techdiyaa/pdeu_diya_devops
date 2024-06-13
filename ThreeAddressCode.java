a9e6854290d287430d4ae98019637dfa1176d58d
DiyaPatel <122555986+Techdiyaa@users.noreply.github.com>
2024-06-13 16:14:11 +0530

    Update ThreeAddressCode.java

--- ThreeAddressCode.java
+++ ThreeAddressCode.java
@@ -7,7 +7,7 @@ public class ThreeAddressCode {
         float d = 4.0f;
         float e = 5.0f;
 
-        // Perform computations modifieddddd
+        // Perform computations modifieddddd here i am using fetch command
         float temp1 = a + b;
         float temp2 = temp1 + c;
         float temp3 = temp2 + d;
